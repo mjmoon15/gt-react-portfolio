845e1f11c39cc2c236f444969194ebf25478ecf2
Mark Mooney <mjmoon15@gmail.com>
2020-08-06 16:57:52 -0400

    added css file to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Header from './containers/header'
 import About from './containers/about'
 import Portfolio from './containers/portfolio'
 import Contact from './containers/contact'
 import Footer from './containers/footer'
+import './'
+
 
 function App() {
   return (
@@ -12,12 +14,15 @@ function App() {
     <div>
       
         <Header />
+        <Switch>
+
         
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
        
+        </Switch>
         <Footer />
       
     </div>
--- src/containers/contact.jsx
+++ src/containers/contact.jsx
@@ -2,51 +2,9 @@ import React from 'react';
 
 const contact = () => {
 	return (
-		<div className="container">
-			<div className="header">
-				<h1>Contact</h1>
-			</div>
-			<hr />
-			<div className="row">
-				<div className="col-sm-4">
-					<div className="card" style="width: 18rem;">
-						<div className="card-body">
-							<h5 className="card-title">Contact Me</h5>
-							<a href="https://github.com/mjmoon15">
-								Mark's github profile can be accessed here.
-							</a>
-							<br />
-							<br />
-							<a href="https://www.linkedin.com/in/mark-mooney-5588a350/">
-								Mark's LinkedIn profile can be accessed here.
-							</a>
-							<br />
-							<br />
-							<a href="https://github.com/mjmoon15/gt-bootstrap-portfolio/blob/master/Resume-Mark-Mooney.pdf">
-								Mark's resume can be accessed here.
-							</a>
-							<br />
-							<br />
-							<p>
-								<a href="mailto:mjmoon15@gmail.com">Send me email</a>
-							</p>
-							<br />
-							<br />
-							<p>Want to chat? Call me at 770-361-6366.</p>
-						</div>
-					</div>
-				</div>
-				<div className="col-sm-8">
-					<img
-						src="Assets/IMG_0665 (1).jpeg"
-						className="img-fluid"
-						className="float:right"
-						id="bubble"
-						alt="Hold your breath"
-					/>
-				</div>
-			</div>
-		</div>
+		<div>
+            <h1>this is my contact page</h1>
+        </div>
 	);
 };
 
