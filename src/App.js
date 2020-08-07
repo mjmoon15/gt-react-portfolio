fc92616fc092ee82643931793a2e7aa79436183d
Mark Mooney <mjmoon15@gmail.com>
2020-08-07 09:47:11 -0400

    added process.env to router

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<Router>
+			<Router basename={process.env.PUBLIC_URL}>
 				<div>
 					<Header />
 					<Switch>
-						<Route exact path="/" component={About}>
+						<Route exact path='/' component={About}>
 							<About />
 						</Route>
-						<Route exact path="/about" component={About}>
+						<Route exact path='/about' component={About}>
 							<About />
 						</Route>
-						<Route exact path="/portfolio" component={Portfolio}>
+						<Route exact path='/portfolio' component={Portfolio}>
 							<Portfolio />
 						</Route>
-						<Route exact path="/contact" component={Contact}>
+						<Route exact path='/contact' component={Contact}>
 							<Contact />
 						</Route>
 					</Switch>
