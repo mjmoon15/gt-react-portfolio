ed4fd573780de5caa857a191f5ad91dc02038bb8
Mark Mooney <mjmoon15@gmail.com>
2020-08-06 21:09:19 -0400

    Updates

---
+++ precache-manifest.981d7274ab9812e90ec13146ff649466.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8c500f82d1c542c3bb36aec64840ef",
+    "url": "/gt-react-portfolio/index.html"
+  },
+  {
+    "revision": "675d4c1d67a99403a041",
+    "url": "/gt-react-portfolio/static/css/main.55038b64.chunk.css"
+  },
+  {
+    "revision": "bafdaf5df9b924b14433",
+    "url": "/gt-react-portfolio/static/js/2.d4237c8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gt-react-portfolio/static/js/2.d4237c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675d4c1d67a99403a041",
+    "url": "/gt-react-portfolio/static/js/main.35426374.chunk.js"
+  },
+  {
+    "revision": "6bb3a8480cb1f79e5ae1",
+    "url": "/gt-react-portfolio/static/js/runtime-main.c610f4b8.js"
+  },
+  {
+    "revision": "dbdfe27e59c5c00a72cdb929921c4413",
+    "url": "/gt-react-portfolio/static/media/IMG_0558.dbdfe27e.jpg"
+  },
+  {
+    "revision": "d72ae4e6169c77037d0e96cef8b789e2",
+    "url": "/gt-react-portfolio/static/media/IMG_0665.d72ae4e6.jpeg"
+  },
+  {
+    "revision": "d193249de10441fd4ad13c25a49b6965",
+    "url": "/gt-react-portfolio/static/media/cats_against_banana_trees_welcome.d193249d.png"
+  },
+  {
+    "revision": "619358b314af1cd4b703fc958066d7ca",
+    "url": "/gt-react-portfolio/static/media/dailyPlanner.619358b3.png"
+  }
+]);
